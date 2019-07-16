8154f3155e2ef789a869774f90da12e1b80383b8
john le <jcle1@uci.edu>
2019-07-16 11:49:41 -0700

    part 2.4 + 2.5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// part 2.2 exercises, works for 2.3 as well 
+// part 2.4, 2.5 done , already made it a seperate module imported by app
 
 // 0 acts as the sums, number would be the item for each iteration when we reduce
 const Course = (props) => {
@@ -10,17 +10,19 @@ const Course = (props) => {
             <h1>{props.course.name}</h1>
             <ul>
                 {myCourses.map(course => <li key = {course.id}>{course.name + " " + course.exercises}</li>)} 
-                <li key = "total"> total of {myCourses.reduce(function(sum, course) {
+                <li key = "total"><strong> total of {myCourses.reduce(function(sum, course) {
                 return sum + course.exercises
-              }, 0)} exercises  </li>
+              }, 0)} exercises  </strong></li>
             </ul>
             
         </div>
     )
 }
 const App = () => {
-    const course = {
+  const courses = [
+    {
       name: 'Half Stack application development',
+      id: 1,
       parts: [
         {
           name: 'Fundamentals of React',
@@ -38,17 +40,34 @@ const App = () => {
           id: 3
         },
         {
-            name: 'Redux',
-            exercises: 11,
-            id: 4
-            
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
         }
       ]
     }
+  ]
   
     return (
       <div>
-        <Course course={course} />
+        <Course course={courses[0]} />
+        <Course course={courses[1]} />
       </div>
     )
   }
